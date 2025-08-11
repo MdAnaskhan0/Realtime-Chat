64a87f59565c4af86ac4d7d8404448711fe61e65
MdAnaskhan0 <mdanaskhan.201902037@gmail.com>
2025-08-11 14:08:06 +0600

    Chat Contatiner is in work.

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useChatStore } from '../store/useChatStore';
 
 const ChatContainer = () => {
+  const { message, getMessage, isMessageLoading, selectedUser } = useChatStore();
+
+  useEffect(() => {
+    getMessage(selectedUser._id);
+  }, [selectedUser._id, getMessage])
+
+  if (isMessageLoading) return <div>Loading...</div>
+
   return (
-    <div>ChatContainer</div>
+    <div className='flex-1 flex flex-col overflow-auto'>
+      <ChatHeader />
+
+      <p>Messages...</p>
+
+      <MessageInput />
+    </div>
   )
 }
 
